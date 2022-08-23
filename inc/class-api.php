c40ddda46fb4ea1735e816be851ea42e1724178a
Sébastien SERRE <sebastien@thivinfo.com>
2022-08-23 21:08:37 +0200

    - rename class - add methodes to comunicate with Nuki API

---
+++ inc/class-api.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Nuki\API;
+class api {
+
+	private $apikey;
+	private $settings;
+	public $remote_url;
+	private $smartlockID;
+
+	public function init(){
+		//$this->define_variables();
+	}
+
+	public function __construct(){
+		$this->settings = \get_option( 'nukiwp__settings' );
+		$this->apikey = $this->settings['apikey'];
+		$this->remote_url = 'https://api.nuki.io';
+		$this->smartlockID = $this->get_smartlock_id();
+	}
+
+	public function api_call( $url, $method='get' ){
+		$args = array(
+			'headers'     => array(
+				'Authorization' => 'Bearer ' . $this->apikey,
+			),
+		);
+
+		switch ( $method ) {
+			case 'get':
+			default:
+			$result = wp_remote_get( $url, $args );
+			if ( 200 === (int) wp_remote_retrieve_response_code( $result ) ) {
+				$body = wp_remote_retrieve_body( $result );
+				$result = json_decode( $body, true );
+			}
+			break;
+			case 'post':
+				$result = wp_remote_post( $url, $args );
+				break;
+		}
+		if ( ! empty( $result ) ) {
+			return $result;
+		} else {
+			return false;
+		}
+
+	}
+
+	public function get_smartlock(){
+		$url = $this->remote_url . '/smartlock';
+		$results = $this->api_call( $url, 'get' );
+		return $results;
+	}
+
+	public function get_smartlock_id(){
+		$smartlocks = $this->get_smartlock();
+		foreach ( $smartlocks as $smartlock ) {
+			$this->smartlockID = $smartlock['smartlockId'];
+		}
+		return $this->smartlockID;
+	}
+
+	public function lock(){
+		$args = array(
+			'url' => $this->remote_url,
+			'tool' => 'smartlock',
+			'id' => $this->smartlockID,
+			'action' => 'action/lock',
+		);
+		$url = implode( '/', $args );
+		$result = $this->api_call( $url, 'post' );
+		return $result;
+	}
+
+	public function unlock(){
+		$args = array(
+			'url' => $this->remote_url,
+			'tool' => 'smartlock',
+			'id' => $this->smartlockID,
+			'action' => 'action/unlock',
+		);
+		$url = implode( '/', $args );
+		$result = $this->api_call( $url, 'post' );
+		return $result;
+	}
+}
