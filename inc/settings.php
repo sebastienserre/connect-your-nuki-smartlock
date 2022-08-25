a1f982f489880098277145ee35364c89c65efad1
Sébastien SERRE <sebastien@thivinfo.com>
2022-08-25 07:54:15 +0200

    use getters

--- inc/settings.php
+++ inc/settings.php
@@ -116,7 +116,7 @@ function nukiwp_time_selector( $hour = 'start') {
 	$hours   = array( '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23' );
 	$minutes = array( '00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55' );
 	$nuki = new \Nuki\API\api();
-	$settings = $nuki->settings;
+	$settings = $nuki->get_settings();
     $selected_hour = $settings[$hour . '-autolock'];
 	foreach ( $hours as $hour ) {
 		foreach ( $minutes as $minute ) {
@@ -130,7 +130,7 @@ function nukiwp_time_selector( $hour = 'start') {
 function nukiwp__manage_smartlock(){
     $nuki = new \Nuki\API\api();
     $smartlocks = $nuki->get_smartlock();
-    $settings = $nuki->settings;
+    $settings = $nuki->get_settings();
     $selected_smartlock = $settings['smartlock-managed'];
 	?>
     <select name='nukiwp__settings[smartlock-managed]'>
