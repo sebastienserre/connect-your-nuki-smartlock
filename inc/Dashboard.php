130723a59c819ac06520cf252e6a66e336c9c30d
Sébastien SERRE <sebastien@thivinfo.com>
2022-09-30 08:44:49 +0200

    Escape & sanitize

--- inc/Dashboard.php
+++ inc/Dashboard.php
@@ -1,34 +1,59 @@
 <?php
+/**
+ * Dashboard Widget.
+ *
+ * @package Connect-your-nuki-smartlock
+ */
 
 namespace Nuki\Dashboard;
 
 use Nuki\API\api;
 
+/**
+ * This class implement the Widget Dashboard.
+ */
 class Dashboard {
 
+	/**
+	 * @var $smartlock_id
+	 */
 	public $smartlock_id;
 
-	public function init(){
+	/**
+	 * Initialize Class.
+	 *
+	 * @return void
+	 */
+	public function init() {
 		add_action( 'wp_dashboard_setup', array( $this, 'dashboard_widget' ) );
-        add_action( 'admin_init', array( $this, 'generate_pincode' ) );
-        add_action( 'admin_init', array( $this, 'delete_pincode' ) );
+		add_action( 'admin_init', array( $this, 'generate_pincode' ) );
+		add_action( 'admin_init', array( $this, 'delete_pincode' ) );
 	}
 
+	/**
+	 * Initialize the Dashboard Widget.
+	 *
+	 * @return void
+	 */
 	public function dashboard_widget() {
-		global $wp_meta_boxes;
-		add_meta_box( 'nuki_smartlock', 'Nuki Smartlock', array( $this, 'render_dashboard'), 'dashboard', 'side', 'high' );
+		add_meta_box( 'nuki_smartlock', 'Nuki Smartlock', array( $this, 'render_dashboard' ), 'dashboard', 'side', 'high' );
 	}
 
-	public function render_dashboard(){
+	/**
+	 * Render the Dashboard Widget.
+	 *
+	 * @return void
+	 */
+	public function render_dashboard() {
 		$nuki = new Api();
 		$data = $nuki->get_smartlock_details( $nuki->get_smartlock_id() );
 		$options = get_option( 'nukiwp__settings' );
 
-        // Generate classname for Battery level.
-        $batteryState = 'ok';
-        if ( $data["state"]["batteryCritical"] ){
-            $batteryState = 'critical';
-        }
+		// Generate classname for Battery level.
+		$battery_state = 'ok';
+		if ( $data['state']['batteryCritical'] ) {
+			$battery_state = 'critical';
+		}
 
 		$generate_link = add_query_arg(
 			array(
@@ -40,72 +65,78 @@ class Dashboard {
 		);
 
 		?>
-        <div class="nuki_dashboard">
-            <h3>
+		<div class="nuki_dashboard">
+			<h3>
 				<?php
 				/* translators: %1$s is the smartlock name defined by customer */
-				printf( __( 'Smartlock managed: %1$s ', 'connect-your-nuki-smartlock' ), $data["name"] );
+				printf( esc_attr__( 'Smartlock managed: %1$s ', 'connect-your-nuki-smartlock' ), esc_attr( $data['name'] ) );
 				?>
-            </h3>
-            <p>
+			</h3>
+			<p>
 				<?php
 				/* translators: %1$s is the smartlock ID */
-				printf( __( '<span>Id:</span> %1$s ', 'connect-your-nuki-smartlock' ), $nuki->get_smartlock_id() );
+				printf( wp_kses( '<span>Id:</span> %1$s ', 'connect-your-nuki-smartlock', array( 'span' ) ), esc_attr( $nuki->get_smartlock_id() ) );
 				?>
-            </p>
-            <p class="battery-<?php echo $batteryState ?>">
+			</p>
+			<p class="battery-<?php echo esc_attr( $battery_state ); ?>">
 				<?php
 				/* translators: %1$s is the level in digit 0->100 */
-				printf( __( '<span>Battery level:</span> %1$s%2$s ', 'connect-your-nuki-smartlock' ), $data["state"]["batteryCharge"], '%' );
+				printf( wp_kses( '<span>Battery level:</span> %1$s%2$s ', 'connect-your-nuki-smartlock', array( 'span' ) ), esc_attr( $data['state']['batteryCharge'] ), '%' );
 				?>
-            </p>
-            <p class="smartlock-<?php echo $nuki->state( $data["state"]["state"], $data["type"] );?>">
-		        <?php
-		        /* translators: %1$s is the smartlock state */
-		        printf( __( '<span>State:</span> %1$s ', 'connect-your-nuki-smartlock' ), $nuki->state( $data["state"]["state"], $data["type"] ) );
-		        ?>
-            </p>
-            <p>
-                <a href="<?php echo $generate_link; ?>"><?php _e( 'Generate a pincode', 'connect-your-nuki-smartlock' ); ?></a>
-            <h4>
-		        <?php
-		        /* translators: %1$s is the smartlock name defined by customer */
-		        printf( __( 'List of On-Demand Pin generated ', 'connect-your-nuki-smartlock' ), $data["name"] );
-		        ?>
-            </h4>
-                <table>
-                <tr>
-                    <th><?php _e('Name', 'connect-your-nuki-smartlock' )?></th>
-                    <th><?php _e('Pincode', 'connect-your-nuki-smartlock' )?></th>
-                    <th><?php _e('Actions', 'connect-your-nuki-smartlock' )?></th>
-                </tr>
-                <?php
-                foreach ( $options['ondemand-pinlist'] as $name => $pin ){
-                 ?>
-                    <tr>
-                        <td><?php echo $name; ?></td>
-                        <td><?php echo $pin; ?></td>
-                        <?php
-                        $delete_link = add_query_arg( array(
-	                        'action'   => 'delete-pin',
-	                        '_wpnonce' => wp_create_nonce( 'action' ),
-                            'pin_name' => $name,
-                        ),
-	                        admin_url( '/' ),
-                        );
-                        ?>
-                        <td><a href="<?php echo $delete_link ?>"><?php _e('Delete', 'connect-your-nuki-smartlock' )?></a></td>
-                    </tr>
-                        <?php
-                }
-                ?>
-            </table>
-            </p>
-        </div>
-<?php
+			</p>
+			<p class="smartlock-<?php echo esc_attr( $nuki->state( $data['state']['state'], $data['type'] ) ); ?>">
+				<?php
+				/* translators: %1$s is the smartlock state */
+				printf( wp_kses( '<span>State:</span> %1$s ', 'connect-your-nuki-smartlock', array( 'span' ) ), esc_attr( $nuki->state( $data['state']['state'], $data['type'] ) ) );
+				?>
+			</p>
+			<p>
+				<a href="<?php echo esc_url( $generate_link ); ?>"><?php esc_html_e( 'Generate a pincode', 'connect-your-nuki-smartlock' ); ?></a>
+			<h4>
+				<?php
+				/* translators: %1$s is the smartlock name defined by customer */
+				printf( esc_attr__( 'List of On-Demand Pin generated ', 'connect-your-nuki-smartlock' ), esc_attr( $data['name'] ) );
+				?>
+			</h4>
+				<table>
+				<tr>
+					<th><?php esc_html_e( 'Name', 'connect-your-nuki-smartlock' ); ?></th>
+					<th><?php esc_html_e( 'Pincode', 'connect-your-nuki-smartlock' ); ?></th>
+					<th><?php esc_html_e( 'Actions', 'connect-your-nuki-smartlock' ); ?></th>
+				</tr>
+				<?php
+				foreach ( $options['ondemand-pinlist'] as $name => $pin ) {
+					?>
+					<tr>
+						<td><?php echo esc_html( $name ); ?></td>
+						<td><?php echo esc_html( $pin ); ?></td>
+						<?php
+						$delete_link = add_query_arg(
+							array(
+								'action'   => 'delete-pin',
+								'_wpnonce' => wp_create_nonce( 'action' ),
+								'pin_name' => $name,
+							),
+							admin_url( '/' ),
+						);
+						?>
+						<td><a href="<?php esc_url( $delete_link ); ?>"><?php esc_html_e( 'Delete', 'connect-your-nuki-smartlock' ); ?></a></td>
+					</tr>
+						<?php
+				}
+				?>
+			</table>
+			</p>
+		</div>
+		<?php
 	}
 
-	function generate_pincode() {
+	/**
+	 * Generate a pincode.
+	 *
+	 * @return void
+	 */
+	public function generate_pincode() {
 		$nuki = new Api();
 		if ( empty( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'action' ) ) {
 			return;
@@ -120,18 +151,23 @@ class Dashboard {
 
 	}
 
-	function delete_pincode() {
+	/**
+	 * Delete a pincode.
+	 *
+	 * @return void
+	 */
+	public function delete_pincode() {
 		if ( empty( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'action' ) ) {
 			return;
 		}
-		if ( empty( $_GET['action'] ) || 'delete-pin' !== $_GET['action'] || empty( $_GET['pin_name'] )  ) {
+		if ( empty( $_GET['action'] ) || 'delete-pin' !== $_GET['action'] || empty( $_GET['pin_name'] ) ) {
 			return;
 		}
 		$options = get_option( 'nukiwp__settings' );
-        if ( isset( $options['ondemand-pinlist'][esc_attr( $_GET['pin_name'])] ) ) {
-	        unset( $options['ondemand-pinlist'][ esc_attr( $_GET['pin_name'] ) ] );
-	        update_option( 'nukiwp__settings', $options );
-        }
+		if ( isset( $options['ondemand-pinlist'][ sanitize_text_field( $_GET['pin_name'] ) ] ) ) {
+			unset( $options['ondemand-pinlist'][ sanitize_text_field( $_GET['pin_name'] ) ] );
+			update_option( 'nukiwp__settings', $options );
+		}
 
 	}
 }
--- inc/settings.php
+++ inc/settings.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Nuki Smartlock Settings.
+ *
+ * @package Connect-your-nuki-smartlock
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -7,25 +12,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'admin_menu', 'nukiwp__add_admin_menu' );
 add_action( 'admin_init', 'nukiwp__settings_init' );
 
-function nukiwp__add_admin_menu(  ) {
+/**
+ * Add admin menu.
+ *
+ * @return void
+ */
+function nukiwp__add_admin_menu() {
 
-	add_menu_page( __('Nuki Settings', 'connect-your-nuki-smartlock' ), __('Nuki Settings', 'connect-your-nuki-smartlock' ), 'manage_options', 'connect_your_nuki_smartlock', 'nukiwp__options_page' );
+	add_menu_page( __( 'Nuki Settings', 'connect-your-nuki-smartlock' ), __( 'Nuki Settings', 'connect-your-nuki-smartlock' ), 'manage_options', 'connect_your_nuki_smartlock', 'nukiwp__options_page' );
 
 }
 
-
-function nukiwp__settings_init(  ) {
+/**
+ * Init settings.
+ *
+ * @return void
+ */
+function nukiwp__settings_init() {
 
 	$options = get_option( 'nukiwp__settings' );
-    $token = $options['apikey'];
+	$token = $options['apikey'];
 	register_setting( 'pluginPage', 'nukiwp__settings' );
+
 
 	add_settings_section(
 		'nukiwp__pluginPage_section',
-		/*__( 'Your section description', 'connect-your-nuki-smartlock' ),*/
-        '',
-		/*'nukiwp__settings_section_callback',*/
-        '',
+		'',
+		'',
 		'pluginPage'
 	);
 
@@ -37,34 +50,39 @@ function nukiwp__settings_init(  ) {
 		'nukiwp__pluginPage_section'
 	);
 
-    if ( ! empty( $token ) ) {
-	    add_settings_field(
-		    'nuki-smartlcok-managed',
-		    __( 'Smartlock to manage', 'connect-your-nuki-smartlock' ),
-		    'nukiwp__manage_smartlock',
-		    'pluginPage',
-		    'nukiwp__pluginPage_section'
-	    );
+	if ( ! empty( $token ) ) {
+		add_settings_field(
+			'nuki-smartlcok-managed',
+			__( 'Smartlock to manage', 'connect-your-nuki-smartlock' ),
+			'nukiwp__manage_smartlock',
+			'pluginPage',
+			'nukiwp__pluginPage_section'
+		);
 
-	    add_settings_field(
-		    'nuki-smartlovk-autolock-activated',
-		    __( 'Enable autolock', 'connect-your-nuki-smartlock' ),
-		    'nukiwp_enable_autolock',
-		    'pluginPage',
-		    'nukiwp__pluginPage_section'
-	    );
+		add_settings_field(
+			'nuki-smartlovk-autolock-activated',
+			__( 'Enable autolock', 'connect-your-nuki-smartlock' ),
+			'nukiwp_enable_autolock',
+			'pluginPage',
+			'nukiwp__pluginPage_section'
+		);
 
-	    add_settings_field(
-		    'nuki-open-auto-lock',
-		    __( 'Autolock between', 'connect-your-nuki-smartlock' ),
-		    'nukiwp__open_action',
-		    'pluginPage',
-		    'nukiwp__pluginPage_section'
-	    );
-    }
+		add_settings_field(
+			'nuki-open-auto-lock',
+			__( 'Autolock between', 'connect-your-nuki-smartlock' ),
+			'nukiwp__open_action',
+			'pluginPage',
+			'nukiwp__pluginPage_section'
+		);
+	}
 }
 
-function nukiwp_apikey_render(  ) {
+/**
+ * API Key field render.
+ *
+ * @return void
+ */
+function nukiwp_apikey_render() {
 
 	$options = get_option( 'nukiwp__settings' );
 	?>
@@ -73,24 +91,34 @@ function nukiwp_apikey_render(  ) {
 
 }
 
-function nukiwp_enable_autolock(  ) {
+/**
+ * Enable Autlock setting.
+ *
+ * @return void
+ */
+function nukiwp_enable_autolock() {
 
 	$options = get_option( 'nukiwp__settings' );
-    if ( empty( $options['autolock_activated'] ) ){
-	    $options['autolock_activated'] = '0';
-    }
+	if ( empty( $options['autolock_activated'] ) ) {
+		$options['autolock_activated'] = '0';
+	}
 	?>
-    <input type='checkbox' name='nukiwp__settings[autolock_activated]' value='1' <?php checked( $options['autolock_activated'], '1') ?>>
+	<input type='checkbox' name='nukiwp__settings[autolock_activated]' value='1' <?php checked( $options['autolock_activated'], '1' ); ?>>
 	<?php
 
 }
 
-function nukiwp__options_page(  ) {
+/**
+ * Render option page.
+ *
+ * @return void
+ */
+function nukiwp__options_page() {
 
 	?>
 	<form action='options.php' method='post'>
 
-		<h2><?php _e( 'Connect your Nuki SmartLock', 'connect-your-nuki-smartlock'); ?></h2>
+		<h2><?php esc_html_e( 'Connect your Nuki SmartLock', 'connect-your-nuki-smartlock' ); ?></h2>
 
 		<?php
 		settings_fields( 'pluginPage' );
@@ -103,58 +131,79 @@ function nukiwp__options_page(  ) {
 
 }
 
-function nukiwp__open_action(){
-    $options = nukiwp_time_selector();
-?>
-    <select name='nukiwp__settings[start-autolock]'>
-        <?php
-        foreach ( $options as $option ){
-            echo $option; // value escaped in nukiwp_time_selector()
-        }
-        ?>
-    </select>
-<?php
-    $options = '';
-    esc_attr_e( 'and', 'connect-your-nuki-smartlock');
-	$options = nukiwp_time_selector( 'end' );
+/**
+ * Display time to run.
+ *
+ * @return void
+ */
+function nukiwp__open_action() {
+	$options = nukiwp_time_selector();
 	?>
-    <select name='nukiwp__settings[end-autolock]'>
+	<select name='nukiwp__settings[start-autolock]'>
 		<?php
-		foreach ( $options as $option ){
+		foreach ( $options as $option ) {
+			// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 			echo $option; // value escaped in nukiwp_time_selector()
 		}
 		?>
-    </select>
+	</select>
+	<?php
+	$options = '';
+	esc_attr_e( 'and', 'connect-your-nuki-smartlock' );
+	$options = nukiwp_time_selector( 'end' );
+	?>
+	<select name='nukiwp__settings[end-autolock]'>
+		<?php
+		foreach ( $options as $option ) {
+
+			// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo $option; // value escaped in nukiwp_time_selector()
+		}
+		?>
+	</select>
 	<?php
 }
 
-function nukiwp_time_selector( $hour = 'start') {
+/**
+ * Create the time selector.
+ *
+ * @param string $hour 'start' or  'end' time selector.
+ *
+ * @return array
+ */
+function nukiwp_time_selector( $hour = 'start' ) {
 	$hours   = array( '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23' );
 	$minutes = array( '00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55' );
 	$nuki = new \Nuki\API\Api();
 	$settings = $nuki->get_settings();
-    $selected_hour = $settings[$hour . '-autolock'];
+	$selected_hour = $settings[ $hour . '-autolock' ];
+	$options = array();
 	foreach ( $hours as $hour ) {
 		foreach ( $minutes as $minute ) {
-            $formatted_hour = $hour . ':' . $minute;
-			$options[] = '<option value="'. esc_attr( $formatted_hour ). '"' . selected( $selected_hour, $formatted_hour, false ). '>' . $formatted_hour . '</option>';
+			$formatted_hour = $hour . ':' . $minute;
+			$options[] = '<option value="' . esc_attr( $formatted_hour ) . '"' . selected( $selected_hour, $formatted_hour, false ) . '>' . $formatted_hour . '</option>';
 		}
 	}
-    return $options;
+	return $options;
 }
 
-function nukiwp__manage_smartlock(){
-    $nuki = new \Nuki\API\Api();
-    $smartlocks = $nuki->get_smartlock();
-    $settings = $nuki->get_settings();
-    $selected_smartlock = $settings['smartlock-managed'];
+/**
+ * Display which Smartllock will be managed.
+ *
+ * @return void
+ */
+function nukiwp__manage_smartlock() {
+	$nuki = new \Nuki\API\Api();
+	$smartlocks = $nuki->get_smartlock();
+	$settings = $nuki->get_settings();
+	$selected_smartlock = $settings['smartlock-managed'];
 	?>
-    <select name='nukiwp__settings[smartlock-managed]'>
+	<select name='nukiwp__settings[smartlock-managed]'>
 		<?php
-		foreach ( $smartlocks as $smartlock ){
-			echo '<option value="'. esc_attr( $smartlock['smartlockId'] ) . '"' . selected( $selected_smartlock, $smartlock['smartlockId'], false ). '>' . $smartlock['name'] . '</option>';
+		foreach ( $smartlocks as $smartlock ) {
+			echo '<option value="' . esc_attr( $smartlock['smartlockId'] ) . '"' . selected( $selected_smartlock, $smartlock['smartlockId'], false ) . '>' . $smartlock['name'] . '</option>';
 		}
 		?>
-    </select>
+	</select>
 	<?php
 }
